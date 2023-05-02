e9ed82b850782692c8297d58268c7aa7d9e3f23d
mahmudremal <mahmudremal@yahoo.com>
2023-05-02 14:02:19 +0600

    Removed bulk assets

--- advanced-gpt3-cv-builder/templates/dashboard/admin/content.php
+++
@@ -1,9 +0,0 @@
-<?php
-$dashboard_permalink = apply_filters( 'futurewordpress/project/advancedgpt3cvbuilder/system/getoption', 'permalink-dashboard', 'dashboard' );
-$dashboard_permalink = site_url( $dashboard_permalink );
-?>
-<?php do_action( 'futurewordpress/project/advancedgpt3cvbuilder/parts/call', 'before_homecontent' ); ?>
-<div>
-    <?php do_action( 'futurewordpress/project/advancedgpt3cvbuilder/parts/call', 'homecontent' ); ?>
-</div>
-<?php do_action( 'futurewordpress/project/advancedgpt3cvbuilder/parts/call', 'after_homecontent' ); ?>
